741ceed448b13b0e7ba8f3d80b08d33d596a848e
Administrator <partytimeegg@gmail.com>
2021-12-16 16:05:04 +0800

    v1.0.0

---
+++ src/com/egg/test/TestAll.java
@@ -0,0 +1,38 @@
+package com.egg.test;
+
+import org.junit.Test;
+
+public class TestAll {
+    @Test
+    public void test01(){
+
+    }
+    @Test
+    public void test02(){
+
+    }
+    @Test
+    public void test03(){
+
+    }
+    @Test
+    public void test04(){
+
+    }
+    @Test
+    public void test05(){
+
+    }
+    @Test
+    public void test06(){
+
+    }
+    @Test
+    public void test07(){
+
+    }
+    @Test
+    public void test08(){
+
+    }
+}
